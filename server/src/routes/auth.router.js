8ae06aa2f96070c58d1d52f003b4518949923af3
sourisagee <serdeshko@icloud.com>
2025-11-26 18:36:07 +0300

    routes done

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,12 +1,12 @@
-const authRouter = require('express').Router()
+const authRouter = require('express').Router();
+const UserController = require('../controllers/user.controller');
 
 authRouter
-    // .get('/refreshTokens', )
-    .post('/signup', )
-    .post('/signin', )
-    .delete('/signout', )
+  // .get('/refreshTokens', UserController.refreshTokens)
+  .post('/signup', UserController.signUp)
+  .post('/signin', UserController.signIn)
+  .delete('/signout', UserController.signOut);
 
-authRouter.route('/likes')
-    .get() // получить все лайки одного пользователя (user get one include word through like)
+authRouter.route('/likes').get(UserController.getAllUserLikes); // получить все лайки одного пользователя (user get one include word through like)
 
-module.exports = authRouter
+module.exports = authRouter;
