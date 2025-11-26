7cb27d5d1c9c41b595113cf16b7866edd9c8541a
sourisagee <serdeshko@icloud.com>
2025-11-26 18:36:00 +0300

    blank for services, controllers

---
+++ server/src/controllers/word.controller.js
@@ -0,0 +1,22 @@
+// все, связанное с карточками
+const WordService = require('../services/word.service');
+
+class WordController {
+  static async getAllWordCards(req, res) {}
+
+  static async createWordCard(req, res) {}
+
+  static async getWordCardById(req, res) {}
+
+  static async updateWordCard(req, res) {}
+
+  static async deleteWordCard(req, res) {}
+
+  static async getAllWordCardLikes(req, res) {}
+
+  static async createNewLike(req, res) {}
+
+  static async removeLike(req, res) {}
+}
+
+module.exports = WordController;
